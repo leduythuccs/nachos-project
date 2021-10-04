31d403deb0bbf513a5ae5d73d85fcf44b90bd156
Le Duy Thuc <leduythuccs@mail.com>
2021-10-04 15:46:44 +0000

    Init commit

---
+++ code/userprog/ksyscall.h
@@ -0,0 +1,35 @@
+/**************************************************************
+ *
+ * userprog/ksyscall.h
+ *
+ * Kernel interface for systemcalls 
+ *
+ * by Marcus Voelp  (c) Universitaet Karlsruhe
+ *
+ **************************************************************/
+
+#ifndef __USERPROG_KSYSCALL_H__ 
+#define __USERPROG_KSYSCALL_H__ 
+
+#include "kernel.h"
+
+
+
+
+void SysHalt()
+{
+  kernel->interrupt->Halt();
+}
+
+
+int SysAdd(int op1, int op2)
+{
+  return op1 + op2;
+}
+
+
+
+
+
+
+#endif /* ! __USERPROG_KSYSCALL_H__ */
