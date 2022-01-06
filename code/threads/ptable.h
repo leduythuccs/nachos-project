15c69cb447587c43cfccd5aaec2039b1bfb55e61
Tran Duy Tien <tien@instance-1.asia-southeast1-b.c.bright-antonym-329412.internal>
2022-01-06 10:29:56 +0000

    rework nachos for multiprocessing

---
+++ code/threads/ptable.h
@@ -0,0 +1,27 @@
+#ifndef PTABLE_H
+#define PTABLE_H
+#include "bitmap.h"
+#include "pcb.h"
+// #include "synch.h"
+
+#define MAX_PROCESS 10
+
+class PTable{
+private:
+    Bitmap* bm;
+    PCB* pcb[MAX_PROCESS];
+    int psize;
+    Semaphore* bmsem;
+public:
+    PTable(int size){};
+    ~PTable(){};
+    int ExecUpdate(char *name);
+    int ExitUpdate(int ec);
+    int JoinUpdate(int id);
+    int GetFreeSlot();
+    bool IsExist(int pid);
+    void Remove(int pid);
+    char* GetFileName(int id);
+};
+
+#endif
---
+++ code/threads/stable.h
@@ -0,0 +1,16 @@
+#include "bitmap.h"
+#include "sem.h"
+#define MAX_SEMAPHORE 10
+
+class STable{
+private:
+    Bitmap *bm;
+    Sem* semTab[MAX_SEMAPHORE];
+public:
+    STable(){};
+    ~STable(){};
+    int Create(char* name, int init);
+    int Wait(char* name);
+    int Signal(char* name);
+    int FindFreeSlot(int id);
+};  
